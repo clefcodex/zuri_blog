d06a68be10248c1cc482d15b862d8cac706b1005
clefcodex <iwewale1@gmail.com>
2021-04-26 21:23:26 +0100

    Added a readme file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Installation Instruction
+
+* [download and install python](https://www.python.org/)
+* [download and install git](https://git-scm.com/downloads)
+
+* clone the repo buy running:
+git clone 
+
+* cd into online-result-checker
+
+* create and activate virtual environment
+
+* run pip install -r requirements.txt
+
+* run python manage.py runserver
+
+* http://localhost:8000/ 
+
+[online](https://myzuriblog.herokuapp.com/)
--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-&enp3qj+bjbm!6mt)k$q2e!1_x0#zt5^+0ybjt36hv@$+9mv!%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myzuriblog.herokuapp.com']
 
 
 # Application definition
@@ -134,3 +135,6 @@ LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+
+django_heroku.settings(locals())
