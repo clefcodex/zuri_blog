f26f8a1c2bf87cb0dacbd26dc3d5571ee15ca99e
clefcodex <iwewale1@gmail.com>
2021-04-28 19:56:01 +0100

    Added django-heroku

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&enp3qj+bjbm!6mt)k$q2e!1_x0#zt5^+0ybjt36hv@$+9mv!%'
+# SECRET_KEY = 'django-insecure-&enp3qj+bjbm!6mt)k$q2e!1_x0#zt5^+0ybjt36hv@$+9mv!%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
